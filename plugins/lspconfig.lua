d5bd9f1c6ed8d5d0b9c68e3a18a13f20006b67c7
chenhonzhou <chenhonzhou@gmail.com>
2023-02-22 22:47:30 +0800

    add tailwindcss

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "tsserver", "volar", "lua_ls" }
+local servers = { "html", "cssls", "tsserver", "volar", "lua_ls", "tailwindcss" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
