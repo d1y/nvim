90d544dc38f11ea26714e29596dfe6cd53e33804
chenhonzhou <chenhonzhou@gmail.com>
2023-03-01 01:56:25 +0800

    add T() function tips

--- commands.lua
+++ commands.lua
@@ -1,5 +1,10 @@
 local create = vim.api.nvim_create_user_command
 
+-- use ~/.zshrc function :)
+-- T ()
+-- {
+--   tmux rename-window $1
+-- }
 create("T", function (args)
   local input = tostring(args.args)
   local cmd = string.format('tmux rename-window %s > /dev/null 2>&1 &', input)
