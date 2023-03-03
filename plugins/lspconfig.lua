dbdbd84abc8ae2715a0990890fe92da6afded2f3
chenhonzhou <chenhonzhou@gmail.com>
2023-03-04 01:47:25 +0800

    add pyright lsp

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- vuels
-local servers = { "html", "cssls", "tsserver", "volar", "lua_ls", "tailwindcss", "gopls" }
+local servers = { "html", "cssls", "tsserver", "volar", "lua_ls", "tailwindcss", "gopls", "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
