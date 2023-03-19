5cedbcc6ceb2866545f9ce3f33cbf03dc3aa3ac6
chenhonzhou <chenhonzhou@gmail.com>
2023-03-19 13:54:48 +0800

    custom codeium plugin mapping

--- plugins/init.lua
+++ plugins/init.lua
@@ -386,7 +386,16 @@ local plugins = {
       vim.keymap.set('n', '<leader>tm', require( 'treesj').toggle)
     end
   },
-  ["Exafunction/codeium.vim"] = { },
+  ["Exafunction/codeium.vim"] = {
+    config = function ()
+      -- https://github.com/Exafunction/codeium.vim
+      vim.g.codeium_disable_bindings = 1
+      vim.keymap.set('i', '<M-Bslash>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+      vim.keymap.set('i', '<M-n>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+      vim.keymap.set('i', '<M-p>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+      vim.keymap.set('n', '<M-c>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    end
+  },
   -- 停留缩进位置
   ["gbprod/stay-in-place.nvim"] = {
     config = function ()
@@ -447,6 +456,5 @@ local plugins = {
 		end,
 	},
 }
-
 
 return plugins
