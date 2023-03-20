c416687e176a74f6e70ec216c0626ca344d4ba14
chenhonzhou <chenhonzhou@gmail.com>
2023-03-20 17:40:27 +0800

    add neovide config

--- chadrc.lua
+++ chadrc.lua
@@ -20,4 +20,13 @@ M.nvimtree = {
 M.plugins = require("custom.plugins")
 M.mappings = require("custom.mappings")
 
+if vim.g.neovide then
+  vim.o.guifont = "Liga SFMono Nerd Font:h14"
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_profiler = false
+  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.g.neovide_cursor_animate_command_line = true
+end
+
 return M
